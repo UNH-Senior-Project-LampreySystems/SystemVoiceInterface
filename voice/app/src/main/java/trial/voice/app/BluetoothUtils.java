d15752b1f0d157c1016f93439428a0b18de9e1e8
nnewdorf <nnewdorf@gmail.com>
2015-11-01 21:50:16 -0500

    added voice storyboard skeleton

---
+++ voice/app/src/main/java/trial/voice/app/BluetoothUtils.java
@@ -0,0 +1,17 @@
+package trial.voice.app;
+
+/**
+ * Created by Nick on 11/1/2015.
+ */
+public class BluetoothUtils
+{
+    public BluetoothUtils()
+    {
+        // empty constructor
+    }
+
+    public String getBluetoothStatus()
+    {
+        return "";
+    }
+}
---
+++ voice/app/src/main/java/trial/voice/app/InternetUtils.java
@@ -0,0 +1,18 @@
+package trial.voice.app;
+
+/**
+ * Created by Nick on 11/1/2015.
+ */
+public class InternetUtils
+{
+    public InternetUtils()
+    {
+        // empty constructor
+    }
+
+    public String getInternetStatus()
+    {
+        String connectionStatus = "You are currently connected to UNH-Secure";
+        return connectionStatus;
+    }
+}
